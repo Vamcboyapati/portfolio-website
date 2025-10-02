040ef586cfbd363169e3c859eecfedbc62e11d22
Vamcboyapati <vamcboyapati246@gmail.com>
2025-10-02 12:23:22 +0530

    check mobile

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+const menuLinks = document.querySelectorAll("#menu a");
+
+menuLinks.forEach(link => {
+  link.addEventListener("click", function() {
+    
+    menuLinks.forEach(l => l.classList.remove("active"));
+
+    this.classList.add("active");
+
+    document.getElementById("hams").checked = false;
+  });
+});
